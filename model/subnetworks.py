bdb5c6b9d7c3b3d582da5fdc67087d1ce5dadf3a
GeoCSBI <geodimas.csbi@gmail.com>
2025-05-09 01:32:39 +0300

    wrapped plottings and interpretaion utilities of subnetworks to a BaseSubnet superclass

--- model/subnetworks.py
+++ model/subnetworks.py
@@ -13,23 +13,65 @@ from skimage.filters import threshold_yen
 from copy import deepcopy
 
 
-class SubnetABC(ABC, nn.Module):
+class BaseSubnet(object):
     
-    @abstractmethod
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError("forward method must be implemented")
+    def __init__(self):
+        # Variables to track intermediate activations
+        self._feature_maps = {}
+        self._last_input = None
+        self._last_output = None
 
-    @abstractmethod
-    def get_prm(self) -> ArrayLike:
-        raise NotImplementedError("get_prm method must be implemented")
+    def get_feature_maps(self) -> Dict[str, torch.Tensor]:
+        """Get all intermediate feature maps."""
+        return self._feature_maps
+
+    def get_last_input(self) -> Optional[torch.Tensor]:
+        """Get the last input tensor."""
+        return self._last_input
+
+    def get_last_output(self) -> Optional[torch.Tensor]:
+        """Get the last output tensor."""
+        return self._last_output
+
+    def refine_prm(self, prm: ArrayLike, height: int, width: int) -> ArrayLike:
+        temp_prm = cv.resize(prm, (width, height), interpolation=cv.INTER_CUBIC)
+        thresh = threshold_yen(prm)
+        temp_prm = (temp_prm > thresh) * temp_prm
+        temp_prm = cv.applyColorMap(temp_prm, cv.COLORMAP_JET)
+        return temp_prm
+
+    def get_prm(self, block_idx: int= 3) -> ArrayLike:
+
+        feature_maps = self._blocks[block_idx - 1].get_feature_maps().detach().cpu().numpy()
+        batch, channels, height, width = feature_maps.shape
+        entropies = []
+
+        if batch > 1:
+            raise ValueError("Batch size must be 1, no implementation yet for batch size > 1")
+
+        for k in range(channels):
+            normalized_feature_map = min_max_normalization(feature_maps[0, k, :, :])
+            channel_entropy = shannon_entropy(normalized_feature_map)
+            entropies.append(channel_entropy)
+
+        sorted_entropies = deepcopy(entropies)
+        sorted_entropies.sort(reverse=True)
+        sorted_entropies = sorted_entropies[:len(sorted_entropies) // 2]
+
+        selected_feature_maps = [min_max_normalization(feature_maps[0, entropies.index(sorted_entropy), :, :]) * 255 for sorted_entropy in sorted_entropies]
+        prm = np.asarray(selected_feature_maps).mean(axis=0).astype(np.uint8)
+
+        return prm
 
 
-class SubnetAVG(nn.Module):
+class SubnetAVG(BaseSubnet, nn.Module):
 
     def __init__(self, 
                  block_config: SubnetworkConfig):
         
-        super(SubnetAVG, self).__init__()
+        nn.Module.__init__(self)
+        BaseSubnet.__init__(self)
+
         self._blocks = nn.ModuleList([ConvSubnetAVGBlock(getattr(block_config, 
                                                                  f"block_{i + 1}")) for i in range(block_config.n_blocks)])
 
@@ -79,45 +121,3 @@ class SubnetAVG(nn.Module):
         self._feature_maps['output'] = x
         
         return x
-
-    def get_feature_maps(self) -> Dict[str, torch.Tensor]:
-        """Get all intermediate feature maps."""
-        return self._feature_maps
-
-    def get_last_input(self) -> Optional[torch.Tensor]:
-        """Get the last input tensor."""
-        return self._last_input
-
-    def get_last_output(self) -> Optional[torch.Tensor]:
-        """Get the last output tensor."""
-        return self._last_output
-
-    def refine_prm(self, prm: ArrayLike, height: int, width: int) -> ArrayLike:
-        temp_prm = cv.resize(prm, (width, height), interpolation=cv.INTER_CUBIC)
-        thresh = threshold_yen(prm)
-        temp_prm = (temp_prm > thresh) * temp_prm
-        temp_prm = cv.applyColorMap(temp_prm, cv.COLORMAP_JET)
-        return temp_prm
-
-    def get_prm(self, block_idx: int= 3) -> ArrayLike:
-
-        feature_maps = self._blocks[block_idx - 1].get_feature_maps().detach().cpu().numpy()
-        batch, channels, height, width = feature_maps.shape
-        entropies = []
-
-        if batch > 1:
-            raise ValueError("Batch size must be 1, no implementation yet for batch size > 1")
-
-        for k in range(channels):
-            normalized_feature_map = min_max_normalization(feature_maps[0, k, :, :])
-            channel_entropy = shannon_entropy(normalized_feature_map)
-            entropies.append(channel_entropy)
-
-        sorted_entropies = deepcopy(entropies)
-        sorted_entropies.sort(reverse=True)
-        sorted_entropies = sorted_entropies[:len(sorted_entropies) // 2]
-
-        selected_feature_maps = [min_max_normalization(feature_maps[0, entropies.index(sorted_entropy), :, :]) * 255 for sorted_entropy in sorted_entropies]
-        prm = np.asarray(selected_feature_maps).mean(axis=0).astype(np.uint8)
-
-        return prm
